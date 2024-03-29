cc04738e0dc88a6fa4f6450b74ed42ad22c9932e
mathorin <mathorin@gmail.com>
2024-03-29 17:50:26 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { AppProvider } from './context/AppContext';
+import CartValue from './components/CartValue';
+import ExpenseList from './components/ExpenseList';
+import ItemSelected from './components/ItemSelected';
+import Location from './components/Location';
+
+function App() {
+  return (
+      <AppProvider>
+          <div className="Container">
+            <CartValue />
+            <ExpenseList />
+            <ItemSelected />
+            <Location />
+        </div>
+      </AppProvider>
+  );
+}
+
+export default App;
