992dc138702f5fc087056c58eac6412ba7f35d62
mathorin <mathorin@gmail.com>
2024-03-29 17:55:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
+import Budget from './components/Budget';
 import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
+import ItemSelected from './components/DepartmentSelected';
 import Location from './components/Location';
 
 function App() {
   return (
       <AppProvider>
           <div className="Container">
+            <h1>Company's Budget Allocation</h1>
+            <Budget />
             <CartValue />
             <ExpenseList />
             <ItemSelected />
