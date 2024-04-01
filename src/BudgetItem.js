516414a0cbb6ecc85cc966bafaa1b41d318c2898
mathorin <mathorin@gmail.com>
2024-04-01 16:07:56 -0600

    Create BudgetItem.js

---
+++ src/BudgetItem.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { FaTimesCircle } from 'react-icons/fa';
+
+const BudgetItem = (props) => {
+    const { dispatch, Location} = useContext(AppContext);
+
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.department,
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.budgetAlloc}</td>
+        <td>{Location}{parseInt(props.unitprice)}</td>
+        <td>{Location}{parseInt(props.budgetAlloc)*parseInt(props.unitprice)}</td>
+        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
+};
+
+export default BudgetItem;
