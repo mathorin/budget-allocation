6e5d49238b6584c5db6e72bb24bfb01957669109
mathorin <mathorin@gmail.com>
2024-04-05 21:38:18 -0600

    Update BudgetOverview.js useState implemented and working for budget

--- src/components/BudgetOverview.js
+++ src/components/BudgetOverview.js
@@ -1,12 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const BudgetOverview = () => {
-    const { budget, expenses, Location, budgety } = useContext(AppContext);
+    // removed budget from here, will want to add back and use useContext
+    const { expenses, Location, budgety } = useContext(AppContext);
     
     // const totalExpenses = expenses.reduce((total, item) => {
     // return (total += (item.unitprice * item.quantity));
     // }, 0);
+
+    const [budget, setBudget] = useState('0');
 
     return (
         <div className='alert alert-primary'>
@@ -19,13 +22,13 @@ const BudgetOverview = () => {
                         step='10' 
                         max={20000}
                         min={0}
+                        onChange={e => setBudget(e.target.value)}
                         >
                     </input>
                 </span>
             </p>
             {/* <span>Budget: {Location}{totalBudget}</span> */}
-            <p><span>Budgety: {budgety - 100}</span></p>
-            <p>Budget: {budget}</p>
+            {/* <p>Budget: {budget}</p> */}
             <p>Expenses: {expenses}</p>
             <p></p>
             <p><span>Remaining: calculated based on changes to budget or spending {budget - expenses}</span></p>
